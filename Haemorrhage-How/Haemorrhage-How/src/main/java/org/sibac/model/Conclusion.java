8ca3b3a5d93cefc9900077a02373e64232af01f7
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2024-05-29 15:44:20 +0100

    .

--- Haemorrhage-How/Haemorrhage-How/src/main/java/org/sibac/model/Conclusion.java
+++ Haemorrhage-How/Haemorrhage-How/src/main/java/org/sibac/model/Conclusion.java
@@ -8,6 +8,10 @@ public class Conclusion extends Fact{
     public static final String REALIZAR_CIRURGIA = "Deve realizar cirurgia";
     public static final String NAO_HA_TRATAMENTO = "Não hã lugar a tratamento";
     public static final String TRATAMENTO_HBP = "Terapia com farmacos|Redução do volume prostatico através de cirurgia|Redução do volume prostatico por vapor de água|Aquabalação";
+    public static final String CANCRO_PROSTATA = "Detetado cancro da próstata";
+    public static final String PEDRAS_RINS = "Detetadas pedras nos rins";
+    public static final String HBP_ANORMAL = "Presença de HBP anormal";
+    public static final String HBP_NORMAL = "Presença de HBP normal";
     private String description;
 
     public Conclusion(String description) {
